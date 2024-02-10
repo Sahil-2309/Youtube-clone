0688871e33fab899b5834493158328d1960b0af8
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2024-02-11 00:51:10 +0530

    Channel Details

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from '.'
+import { fetchFromAPI } from '../utils/fetchFromApi'
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>
+  const { id } = useParams()
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+  console.log('channelDetail:', channelDetail)
+  console.log('videos:', videos)
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => {
+        if (data && data.items && data.items.length > 0) {
+          setChannelDetail(data.items[0])
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching channel details:', error)
+      })
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => {
+        if (data && data.items) {
+          setVideos(data.items)
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching videos:', error)
+      })
+  }, [id])
+
+  return (
+    <Box minHeight='95vh'>
+      <Box
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          background:
+            'radial-gradient(circle, rgba(28,28,28,1) 29%, rgba(82,105,221,1) 100%)',
+          zIndex: '-1',
+        }}
+      >
+        {channelDetail && (
+          <ChannelCard channelDetail={channelDetail} style={{ zIndex: '3' }} />
+        )}
+      </Box>
+      <Box p={5} display='flex'>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  )
 }
 
 export default ChannelDetail
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Typography } from '@mui/material'
+import { fetchFromAPI } from '../utils/fetchFromApi'
+import { Videos } from './'
 
-const Video = () => {
-  return <div>Video</div>
+const SearchFeed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('Home')
+  const [videos, setVideos] = useState(null)
+
+  useEffect(() => {
+    setVideos(null)
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    )
+  }, [selectedCategory])
+
+  return (
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        {selectedCategory === 'Home' ? 'Home' : selectedCategory}{' '}
+        <span
+          style={{ color: selectedCategory === 'Home' ? 'white' : '#FC1503' }}
+        >
+          {selectedCategory === 'Home' ? '' : 'Videos'}
+        </span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  )
 }
 
-export default Video
+export default SearchFeed
